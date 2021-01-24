2c11eb66035d39d176bc443a8becdcb434a5e406
Juliette Abeyta <juliette.abeyta@evgo.com>
2021-01-24 14:39:21 -0800

    Add about links

--- pages/about.js
+++ pages/about.js
@@ -13,15 +13,20 @@ export default function Home() {
         <div className="about-container">
           <div>
             <img src="neek.jpg" alt="Selfie of Neek" />
-            <p><span>Neek</span> is operating out of Denver as the designer and curator all of the art produced here.
+            <p><span>Neek</span> operates out of Denver as the designer and curator all of the art produced here.
             She intends to be loud and unapologetic about the messages depicted in our pieces and hopes to achieve all of your collaging hopes and dreams.
             When she is not collaging, Neek is either at work for Planned Parenthood, going on mountain adventures, or antiquing her little heart out. She is also
             the loving mother of the world's biggest nuisance of a cat called Nano.</p>
+            <a href="https://www.instagram.com/saintneeek/?hl=en" target="_blank"><img src="instagram-logo.png" alt="instagram logo" /></a>
+            <a href="https://twitter.com/neeekromancer" target="_blank"><img src="twitter-logo.png" alt="twitter logo" /></a>
           </div>
           <div>
             <img src="junie.jpg" alt="Selfie of Junie" /><p><span>Junie </span> is a Los Angeles-based full-time software engineer & sweetie pie who handles
             all of the merch design and business management for WRWG. They have a passion for creative operation in myriad mediums and function to add 
             dimension to the functions of this studio. Outside of slinging code, Junie enjoys reading, cooking good vegan food, and dabbling in piano playing.</p>
+            <a href="https://www.instagram.com/juneauwhoa/?hl=en" target="_blank"><img src="instagram-logo.png" alt="instagram logo" /></a>
+            <a href="https://www.juliette.dev/" target="_blank"><img style={{ filter: 'invert(1)' }} src="website-logo.png" alt="world wide web icon" /></a>
+
           </div>
         </div>
       </div>
