72c8f534609237d463e48f0c61fbb39c950fcf1e
Juliette Abeyta <juliette.abeyta@evgo.com>
2021-10-04 11:19:16 -0700

    Break out intro to its own component, responsively style header

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,12 @@
 import { useEffect, useRef } from 'react';
 
 import Layout from 'layouts/layout';
+import Introduction from 'components/Introduction';
 
 export default function Home() {
-  const text1 = useRef(null);
-  const text2 = useRef(null);
-  useEffect(() => {
-    text1.current.innerHTML = text1.current.textContent.replace(/\S/g, "<span class='char'>$&</span>");
-    text2.current.innerHTML = text1.current.textContent.replace(/\S/g, "<span class='char'>$&</span>");
-  });
   return (
     <Layout>
-      <div className='subtext'>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porta varius est 
-          a laoreet. Integer sit amet euismod mi. Phasellus in est sit amet tellus volutpat 
-          semper.
-        </div>
-      </div>
-      <div className='sidetext'>
-        <div>
-          <p>
-            [they/them]
-          </p>
-        </div>
-      </div>
-      <div className="header">
-        <div>
-          <h2 className="header-img-text" ref={ text1 }>Juliette</h2>
-          <h2 className="header-img-text" ref={ text2 }>Juliette</h2>
-          <img className="header-img" src="juliette.jpeg" alt="Juliette headshot"/>
-        </div>
-        <div>
-          <p>
-            Juliette Spencer is a full stack developer based in Los Angeles, California.
-            They work on a multidisciplinary team of engineers at EVgo. They're very good
-            at writing code, bein' a sweetie pie, and cooking vegan food. 
-          </p>
-        </div>
-      </div>
+      <Introduction />
     </Layout>
   );
 }
