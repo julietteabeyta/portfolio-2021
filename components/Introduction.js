fe86772dde42b93ff0b94f797291a9c0c31958e8
Juliette Abeyta <juliette.abeyta@evgo.com>
2021-10-08 16:54:26 -0700

    Add header svgs

--- components/Introduction.js
+++ components/Introduction.js
@@ -8,7 +8,7 @@ export default function Introduction() {
     text2.current.innerHTML = text1.current.textContent.replace(/\S/g, "<span class='char'>$&</span>");
   });
   return (
-    <Fragment>
+    <div className="introduction-container">
       <div className='header-subtext subtext'>
         <div>
         You're an artist and your canvas is the lives of those you love.
@@ -27,14 +27,17 @@ export default function Introduction() {
           <h2 className="header-img-text" ref={ text2 }>Juliette</h2>
           <img className="header-img" src="juliette.jpeg" alt="Juliette headshot"/>
         </div>
-        <div>
+        <div className="about-container">
           <p>
-            Juliette Abeyta is a full stack developer based in Los Angeles, California.
+            Juliette is a <span className="full-stack-string">full stack developer</span> based in Los Angeles, California.
             They work on a multidisciplinary team of engineers at EVgo. They're very good
-            at writing code, bein' a sweetie pie, and cooking vegan food. 
+            at <span className="writing-code-string">writing code</span>, bein' a sweetie pie, and cooking vegan food.
           </p>
+          <svg className="header-svg arrow" width="188" height="53" viewBox="0 0 188 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M186.5 32.0001C161.803 -7.14034 83.5 -15.0822 1 46.8833M1 46.8833L30.5 51.5M1 46.8833V22.5333" stroke="#86928A" stroke-width="2"/>
+          </svg>
         </div>
       </div>
-    </Fragment>
+    </div>
   );
 }
